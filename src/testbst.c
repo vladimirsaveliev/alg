da22ed962834b8b03f3b46267d21b3200b8b01d8
Vladimir Saveliev <c17830@cray.com>
2020-12-21 01:24:47 +0300

    minor improvement in test code

--- src/testbst.c
+++ src/testbst.c
@@ -2,14 +2,59 @@
 #include <stdio.h>
 #include "lib/bst/bst.h"
 
+#if 1
+void build_pb_bst(struct bst_node **root,  int *keys, int nr_keys)
+{
+	int ind = nr_keys / 2;
+
+	if (nr_keys == 0)
+		return;
+	bst_insert(root, keys[ind]);
+	build_pb_bst(root, keys, ind);
+	build_pb_bst(root, keys + ind + 1, nr_keys - ind - 1);
+}
+#endif
+
 /*
  *
  */
-int main(void)
+int testbst(void)
 {
 	struct bst_node *root;
+
+	root = NULL;
+	while (1) {
+		char c;
+		int key;
+
+		printf("   \x1b[37;1m1 - insert element\x1b[0m\n");
+		printf("   2 - delete element\n");
+		printf("   3 - print BST\n");
+		printf("   0 - return to main menu\n");
+
+		scanf(" %c", &c);
+		switch (c) {
+		case '1':
+			printf("      key to insert: ");
+			scanf(" %d", &key);
+			bst_insert(&root, key);
+			break;
+		case '2':
+			printf("delete\n");
+			break;
+		case '3':
+			printf("\x1B[36;1m");
+			bst_show_tree(root);
+			printf("\x1b[0m");
+			break;
+		case '0':
+			bst_destroy(root);
+			return 0;
+		}
+	}
+#if 0
 /*	int data[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};*/
-	int data[5] = {1, 2, 3, 4, 5};
+	int data[7] = {1, 2, 3, 4, 5, 6, 7};
 /*	int data[5] = {2, 1, 3, 9, 7};*/
 	int i;
 
@@ -17,13 +62,18 @@ int main(void)
 
 
 	root = NULL;
+
+/*	build_pb_bst(&root, data, sizeof(data)/sizeof(data[0]));*/
+
 	for (i = 0; i < sizeof(data)/sizeof(data[0]); i++) {
 		bst_insert(&root, data[i]);
 	}
+
+/*
 	bst_insert(&root, -1);
 	bst_insert(&root, -3);
 	bst_insert(&root, -2);
-
+*/
 	bst_show_tree(root);
 
 
@@ -41,5 +91,6 @@ int main(void)
 
 
 	bst_destroy(root);
+#endif
 	return 0;
 }
