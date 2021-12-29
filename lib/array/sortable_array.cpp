5166d847d6b68ccf3adae6aea4889e3a7ee1695b
Saveliev Mikhail <savelievmv@gmail.com>
2021-12-29 01:24:01 +0100

    Testsortable_array runs QuickSort

--- lib/array/sortable_array.cpp
+++ lib/array/sortable_array.cpp
@@ -1,6 +1,7 @@
-#include "SortedArray.h"
 #include <iostream>
 #include <assert.h>
+#include <math.h>
+#include "sortable_array.h"
 
 using namespace std;
 
@@ -62,156 +63,148 @@ struct arrayx _quick_sort(struct arrayx *x) {
 
 	return *x;
 }
-
-
-class sorted_array
+sorted_array::sorted_array(int sizex)
 {
-private:
-	int* arr;
-	int size;
+	this->size = sizex;
+	this->arr = new int[sizex];
+}
 
-public:
-	sorted_array(int sizex) {
-		this->size = sizex;
-		this->arr = new int[sizex];
+sorted_array::sorted_array(int arrx[], int sizex) {
+	this->size = sizex;
+	this->arr = new int[sizex];
+	for (int i = 0; i < sizex; i++) {
+		this->arr[i] = arrx[i];
 	}
+}
 
-	sorted_array(int arrx[], int sizex) {
-		this->size = sizex;
-		this->arr = new int[sizex];
-		for (int i = 0; i < sizex; i++) {
-			this->arr[i] = arrx[i];
-		}
-	}
-
-	~sorted_array() {
-		delete[] this->arr;
-	}
+sorted_array::~sorted_array() {
+	delete[] this->arr;
+}
 
 	// Properties:
 	//	time complexity O(x^2)
 	//	stable, in-place, data sensitive
-	void bubble_sort() {
-		int end = this->size - 1;
-		int changed = 1;
-		while (changed == 1) {
-			changed = 0;
-			for (int i = 0; i < end; i++) {
-				if (arr[i] > arr[i + 1]) {
-					int tmp = arr[i + 1];
-					arr[i + 1] = arr[i];
-					arr[i] = tmp;
-					changed = 1;
-				}
+void sorted_array::bubble_sort() {
+	int end = this->size - 1;
+	int changed = 1;
+	while (changed == 1) {
+		changed = 0;
+		for (int i = 0; i < end; i++) {
+			if (arr[i] > arr[i + 1]) {
+				int tmp = arr[i + 1];
+				arr[i + 1] = arr[i];
+				arr[i] = tmp;
+				changed = 1;
 			}
-			end--;
+		}
+		end--;
+	}
+}
+
+// Properties:
+//	time complexity O(n^2)
+//	in-place, not data sensitive, not stable
+void sorted_array::select_sort() {
+	for (int i = 0; i < this->size; i++) {
+		int ind = i;
+		for (int j = i + 1; j < this->size; j++) {
+			if (arr[j] < arr[ind]) {
+				ind = j;
+			}
+		}
+		int tmp = arr[i];
+		arr[i] = arr[ind];
+		arr[ind] = tmp;
+	}
+}
+
+// Properties:
+//	time complexity O(n^2)
+//	in-place, data sensitive, stable
+void sorted_array::insert_sort() {
+	for (int i = 1; i < this->size; i++) {
+		int key = arr[i];
+		int j = i - 1;
+		while ((j >= 0) && (arr[j] > key)) {
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+		arr[j + 1] = key;
+	}
+}
+
+void sorted_array::merge_sort() {
+
+}
+
+// Design an algorithm which finds out the number 
+// of inverses in the input sequence.
+// Ex 2. Tutorial 5
+int sorted_array::num_of_inverses() {
+	int count = 0;
+	for (int i = 1; i <= this->size; i++) {
+		for (int j = i + 1; j <= this->size; j++) {
+			if (arr[i] > arr[j]) {
+				count++;
+			}
 		}
 	}
+	
+	return count;
+}
 
-	// Properties:
-	//	time complexity O(n^2)
-	//	in-place, not data sensitive, not stable
-	void select_sort() {
+// Array elements are {0 <= this->arr[i] <= this->size^3 - 1}
+void sorted_array::radix_sort() {
+	int *count = new int[this->size];
+	int* output = new int[this->size];
+	for (int k = 0; k < 3; k++) {
+		int div = pow(this->size, k);
 		for (int i = 0; i < this->size; i++) {
-			int ind = i;
-			for (int j = i + 1; j < this->size; j++) {
-				if (arr[j] < arr[ind]) {
-					ind = j;
-				}
-			}
-			int tmp = arr[i];
-			arr[i] = arr[ind];
-			arr[ind] = tmp;
+			count[i] = 0;
 		}
-	}
-
-	// Properties:
-	//	time complexity O(n^2)
-	//	in-place, data sensitive, stable
-	void insert_sort() {
+		for (int i = 0; i < this->size; i++) {
+			int rem = ((this->arr[i] / div) % this->size);
+			count[rem]++;
+		}
 		for (int i = 1; i < this->size; i++) {
-			int key = arr[i];
-			int j = i - 1;
-			while ((j >= 0) && (arr[j] > key)) {
-				arr[j + 1] = arr[j];
-				j = j - 1;
-			}
-			arr[j + 1] = key;
+			count[i] += count[i - 1];
 		}
-	}
-
-	void merge_sort() {
-
-	}
-
-	// Design an algorithm which finds out the number 
-	// of inverses in the input sequence.
-	// Ex 2. Tutorial 5
-	int num_of_inverses() {
-		int count = 0;
-		for (int i = 1; i <= this->size; i++) {
-			for (int j = i + 1; j <= this->size; j++) {
-				if (arr[i] > arr[j]) {
-					count++;
-				}
-			}
+		// Build output array
+		for (int i = this->size - 1; i >= 0; i--) {
+			int rem = ((this->arr[i] / div) % this->size);
+			int pos = count[rem] - 1;
+			output[pos] = this->arr[i];
+			count[rem]--;
 		}
-
-		return count;
-	}
-
-	// Array elements are {0 <= this->arr[i] <= this->size^3 - 1}
-	void radix_sort() {
-		int *count = new int[this->size];
-		int* output = new int[this->size];
-		for (int k = 0; k < 3; k++) {
-			int div = pow(this->size, k);
-			for (int i = 0; i < this->size; i++) {
-				count[i] = 0;
-			}
-			for (int i = 0; i < this->size; i++) {
-				int rem = ((this->arr[i] / div) % this->size);
-				count[rem]++;
-			}
-			for (int i = 1; i < this->size; i++) {
-				count[i] += count[i - 1];
-			}
-			// Build output array
-			for (int i = this->size - 1; i >= 0; i--) {
-				int rem = ((this->arr[i] / div) % this->size);
-				int pos = count[rem] - 1;
-				output[pos] = this->arr[i];
-				count[rem]--;
-			}
 			// Copy output array to P
-			for (int i = 0; i < this->size; i++) {
-				this->arr[i] = output[i];
-			}
-			this->print_array();
-		}
-	}
-
-	void randomizer(int min, int max) {
 		for (int i = 0; i < this->size; i++) {
-			this->arr[i] = range_rand(min, max);
+			this->arr[i] = output[i];
 		}
+		this->print_array();
 	}
+}
 
-	void quick_sort() {
-		struct arrayx x;
-		x.arr = this->arr;
-		x.size = this->size;
-		_quick_sort(&x);
+void sorted_array::randomizer(int min, int max) {
+	for (int i = 0; i < this->size; i++) {
+		this->arr[i] = range_rand(min, max);
 	}
+}
 
-	void print_array() {
-		for (int i = 0; i < size; i++) {
-			cout << arr[i] << " ";
-		}
-		cout << "\n";
+void sorted_array::quick_sort() {
+	struct arrayx x;
+	x.arr = this->arr;
+	x.size = this->size;
+	_quick_sort(&x);
+}
+
+void sorted_array::print_array() {
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << " ";
 	}
-};
+	cout << "\n";
+}
 
+/*
 int main() {
 	sorted_array new_arr = sorted_array(10);
 	new_arr.randomizer(0, pow(10, 3) - 1);
@@ -221,3 +214,4 @@ int main() {
 
 	return 0;
 }
+*/
--- src/testsortable_array.cpp
+++ src/testsortable_array.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "lib/array/sortable_array.h"
 
 using namespace std;
 
@@ -7,9 +8,13 @@ extern "C" {
 		while (1) {
 			char c;
 			int key;
+			sorted_array arr(100);
+			arr.randomizer(-1024, 1024);
+			
 
 			cout << "   \x1b[37;1m1 - create array with random numbers\x1b[0m\n";
 			cout << "   2 - sort with radix sort algorithm\n";
+			cout << "   3 - sort with quick sort algorithm\n";
 			cout << "   0 - exit\n";
 
 			cin >> c;
@@ -20,6 +25,10 @@ extern "C" {
 				break;
 			case '2':
 				cout << "Not ready\n";
+				break;
+			case '3':
+				arr.quick_sort();
+				arr.print_array();
 				break;
 			case '0':
 				return 0;
