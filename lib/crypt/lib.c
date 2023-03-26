2e79cbdcd78fe8f40f33bafda4b302f63b220ef9
Vladimir Saveliev <c17830@cray.com>
2023-03-26 15:08:30 +0300

    multiplicative inverse is added

--- lib/crypt/lib.c
+++ lib/crypt/lib.c
@@ -1,8 +1,45 @@
 #include <stdio.h>
 
+struct mi_struct
+{
+    int q;
+    int a;
+    int b;
+    int r;
+    int t1;
+    int t2;
+    int t;
+};
+
 /*
  * multiplicative inverse
+ * a > b
  */
+int mi(int a, int b)
+{
+    struct mi_struct mi;
+    
+    mi.a = a;
+    mi.b = b;
+    mi.t1 = 0;
+    mi.t2 = 1;
+    
+    printf("%4s%4s%4s%4s%4s%4s%4s\n", "Q", "A", "B", "R", "T1", "T2", "T");
+    while (mi.b != 0) {
+        /* calculate q, r, t */
+        mi.q = mi.a / mi.b;
+        mi.r = mi.a % mi.b;
+        mi.t = mi.t1 - mi.t2 * mi.q;
+        printf("%4d%4d%4d%4d%4d%4d%4d\n", mi.q, mi.a, mi.b, mi.r, mi.t1, mi.t2, mi.t);
+        
+        /* shift */
+        mi.a = mi.b;
+        mi.b = mi.r;
+        mi.t1 = mi.t2;
+        mi.t2 = mi.t;
+    }
+    return mi.t1;
+}
 
 /*
  * multiple and square algorithm
--- src/testcrypt.c
+++ src/testcrypt.c
@@ -3,12 +3,11 @@
 
 int testcrypt(void)
 {
-	int nr;
-
 	while (1) {
 		char c;
 
 		printf("   \x1b[37;1m1 - modulo power\x1b[0m\n");
+		printf("   \x1b[37;1m2 - multiplicative inverse\x1b[0m\n");
 		printf("   0 - return to main menu\n");
 
 		scanf(" %c", &c);
@@ -23,6 +22,17 @@ int testcrypt(void)
                         }
                         printf("res %d\n", mod_power(b, p, m));
 			break;
+                    }
+                    case '2': {
+                        int a, b;
+                        
+			printf("      big small: ");
+			if (scanf(" %d %d", &a, &b) != 2 || a <= b) {
+                            printf("valid input example: 20 15\n");
+                            continue;
+                        }
+                        printf("res %d\n", mi(a, b));
+                        break;
                     }
                     case '0':
 			return 0;
